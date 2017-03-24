3ad910f0e979afdab5213ba00820af274f4b402a
David Kuo <s50407s@gmail.com>
2017-03-24 21:05:20 +0800

    Reconnect after connection lost from DB.

--- src/java/org/snowtec/mine/skinny/SkinnySnowServer.java
+++ src/java/org/snowtec/mine/skinny/SkinnySnowServer.java
@@ -1,13 +1,11 @@
 package org.snowtec.mine.skinny;
 
-import org.bukkit.configuration.Configuration;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 /**
  * @author Davy
@@ -16,17 +14,19 @@ public class SkinnySnowServer extends JavaPlugin {
     private static SkinnySnowServer sInstance;
 
     private Connection mConnection;
+    private ConnectionCredential mCredential;
 
     @Override
     public void onEnable() {
         sInstance = this;
         saveDefaultConfig();
 
+        final ConfigurationSection config = getConfig().getConfigurationSection("database");
+        mCredential = new ConnectionCredential(config.getString("host"), config.getString("port"),
+                config.getString("database"),
+                config.getString("username"), config.getString("password"));
         try {
-            final ConfigurationSection config = getConfig().getConfigurationSection("database");
-            mConnection = openConnection(config.getString("host"), config.getString("port"),
-                    config.getString("database"),
-                    config.getString("username"), config.getString("password"));
+            mConnection = openConnection();
         } catch (ClassNotFoundException | SQLException e) {
             mConnection = null;
             getLogger().warning("Cannot connect to MySQL server.");
@@ -41,13 +41,37 @@ public class SkinnySnowServer extends JavaPlugin {
     }
 
     public Connection getConnection() {
+        try {
+            if (mConnection == null || mConnection.isClosed()) {
+                getLogger().warning("Connection to MySQL server closed. Reconnecting...");
+                mConnection = openConnection();
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            mConnection = null;
+            getLogger().warning("Cannot connect to MySQL server.");
+            e.printStackTrace();
+        }
+
         return mConnection;
     }
 
-    private Connection openConnection(final String host, final String port, final String database,
-                                     final String username, final String password)
-            throws ClassNotFoundException, SQLException {
+    private Connection openConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.jdbc.Driver");
-        return DriverManager.getConnection("jdbc:mysql://" + host+ ":" + port + "/" + database, username, password);
+        return DriverManager.getConnection("jdbc:mysql://" +
+                mCredential.host+ ":" + mCredential.port + "/" + mCredential.database,
+                mCredential.username, mCredential.password);
+    }
+
+    class ConnectionCredential {
+        final String host, port, database, username, password;
+
+        ConnectionCredential(final String host, final String port, final String database,
+                             final String username, final String password) {
+            this.host = host;
+            this.port = port;
+            this.database = database;
+            this.username = username;
+            this.password = password;
+        }
     }
 }
